ce1630fd445f3c476ebad55a16387f2938dd2034
goolbi <pinpinprin1227@gmail.com>
2026-07-11 17:36:03 +0900

    PlayerMove

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = 0f;
+        float vertical = 0f;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            horizontal = -1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            horizontal = 1f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            vertical = -1f;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            vertical = 1f;
+        }
+
+        Vector3 direction = new Vector3(horizontal, vertical, 0f).normalized;
+        transform.position += direction * speed * Time.deltaTime;
     }
 }
